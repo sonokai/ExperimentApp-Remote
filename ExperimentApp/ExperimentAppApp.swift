250877d20229b0d4cc1286d9cf1d442b43a98ae1
sonokai <kaibkg111@gmail.com>
2023-07-07 17:40:23 -0700

    Initial Commit

---
+++ ExperimentApp/ExperimentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperimentAppApp.swift
+//  ExperimentApp
+//
+//  Created by Kai Green on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ExperimentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
