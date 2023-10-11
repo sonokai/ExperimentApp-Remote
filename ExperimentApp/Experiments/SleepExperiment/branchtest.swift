d500967fae640f4e433f535d6f0890f6be06f5f0
bellchen5@gmail.com <bellchen5@gmail.com>
2023-10-10 20:54:13 -0700

    test the branch for branch beta

---
+++ ExperimentApp/Experiments/SleepExperiment/branchtest.swift
@@ -0,0 +1,20 @@
+//
+//  branchtest.swift
+//  ExperimentApp
+//
+//  Created by Bell Chen on 10/10/23.
+//
+
+import SwiftUI
+
+struct branchtest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct branchtest_Previews: PreviewProvider {
+    static var previews: some View {
+        branchtest()
+    }
+}
