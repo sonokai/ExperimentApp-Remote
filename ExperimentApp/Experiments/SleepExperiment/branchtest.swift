85f2ac2cb0fb392e0ee272ef660bea5b85a82100
bellchen5@gmail.com <bellchen5@gmail.com>
2023-10-10 22:18:41 -0700

    does it work

--- ExperimentApp/Experiments/SleepExperiment/branchtest.swift
+++ ExperimentApp/Experiments/SleepExperiment/branchtest.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct branchtest: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Does gitignore work?")
+        
     }
 }
 
